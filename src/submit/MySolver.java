0999637ac5abe15d7aab4aecd6a92e3120bc02dd
xincheng zhang <xinczhan@gmail.com>
2013-02-08 16:53:14 -0800

    initial commit with null check redundancy detection done

---
+++ src/submit/MySolver.java
@@ -0,0 +1,141 @@
+package submit;
+
+import joeq.Compiler.Quad.*;
+import jwutil.graphs.Navigator;
+import flow.Flow;
+import flow.Flow.DataflowObject;
+
+import java.util.*;
+/**
+ * Skeleton class for implementing the Flow.Solver interface.
+ */
+public class MySolver implements Flow.Solver {
+
+    protected Flow.Analysis analysis;
+    /**
+     * Sets the analysis.  When visitCFG is called, it will
+     * perform this analysis on a given CFG.
+     *
+     * @param analyzer The analysis to run
+     */
+    public void registerAnalysis(Flow.Analysis analyzer) {
+        this.analysis = analyzer;
+    }
+
+    /**
+     * Runs the solver over a given control flow graph.  Prior
+     * to calling this, an analysis must be registered using
+     * registerAnalysis
+     *
+     * @param cfg The control flow graph to analyze.
+     */
+    public void visitCFG(ControlFlowGraph cfg) {
+
+        // this needs to come first.
+        analysis.preprocess(cfg);
+        boolean changed = true;
+        boolean buildTerminals=true;
+        Set<Quad> terminals=new HashSet<Quad>();
+        while (changed) {
+        	changed = false;
+        	QuadIterator qit = new QuadIterator(cfg);
+        	while (qit.hasNext()) {
+        		Quad q = qit.next();
+        		Iterator<Quad> meets;
+	        	if (analysis.isForward()){
+	        		meets = qit.predecessors();
+	        	} else {
+	        		meets = qit.successors();       		
+	        	}
+	        	DataflowObject d_obj = null;
+	            while(meets.hasNext()){
+	            		Quad q1 = meets.next();
+	            		if (d_obj == null) {
+	            			d_obj = analysis.newTempVar();
+	            			if (q1 != null) {
+		            			if (analysis.isForward()) {
+		            				d_obj.copy(analysis.getOut(q1));
+		            			} else {
+		            				d_obj.copy(analysis.getIn(q1));
+		            			}
+		            		} else{
+		            			if (analysis.isForward()) {
+		            				d_obj.copy(analysis.getEntry());
+		            			} else {
+		            				d_obj.copy(analysis.getExit());
+		            			}
+	                        }
+	            		} else {
+		            		if (q1 != null) {
+		            			if (analysis.isForward()) {
+		            				d_obj.meetWith(analysis.getOut(q1));
+		            			} else {
+		            				d_obj.meetWith(analysis.getIn(q1));
+		            			}
+		            		} else{
+		            			if (analysis.isForward()) {
+		            				d_obj.meetWith(analysis.getEntry());
+		            			} else {
+		            				d_obj.meetWith(analysis.getExit());
+		            			}
+	                        }
+	            		}
+	        	}
+                if(buildTerminals){
+	        	    if (analysis.isForward()){
+	        		    meets = qit.successors();       		
+	        	    } else {
+	        		    meets = qit.predecessors();
+	        	    }
+                    while(meets.hasNext()){
+	            		Quad qt = meets.next();
+	            		if (qt == null) {
+                            terminals.add(q);
+                        }
+                    }
+                }
+	            DataflowObject old = analysis.newTempVar();
+	            if (analysis.isForward()) {
+	                old.copy(analysis.getOut(q));
+	                analysis.setIn(q, d_obj);
+	            } else {
+	                old.copy(analysis.getIn(q));
+	                analysis.setOut(q, d_obj);
+	            }	               
+	            analysis.processQuad(q);
+                    DataflowObject terminal;
+	        	    if (analysis.isForward()){
+	        		    terminal=analysis.getExit();
+	        	    } else {
+	        		    terminal=analysis.getEntry();
+	        	    }
+                for(Quad qt:terminals){
+	        	    if (analysis.isForward()){
+	        		    terminal.meetWith(analysis.getOut(qt));       		
+	        	    } else {
+	        		    terminal.meetWith(analysis.getIn(qt));       		
+	        	    }
+                }   
+	        	if (analysis.isForward()){
+	        		    analysis.setExit(terminal);
+	        	} else {
+	        		    analysis.setEntry(terminal);
+	        	}
+	            DataflowObject res = analysis.newTempVar();
+	            if (analysis.isForward()) {
+	             	res.copy(analysis.getOut(q));
+	            } else {
+	                res.copy(analysis.getIn(q));    	
+	            }
+	            if (!changed && !old.equals(res)) {
+	                changed = true;
+	            }
+        	}
+                    buildTerminals=false;
+        	
+        }
+
+        // this needs to come last.
+        analysis.postprocess(cfg);
+    }
+}
---
+++ src/submit/NullCheckAnalysis.java
@@ -0,0 +1,271 @@
+package submit;
+
+// some useful things to import. add any additional imports you need.
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+import joeq.Compiler.Quad.*;
+import joeq.Compiler.Quad.Operand.RegisterOperand;
+import flow.Flow;
+import flow.Liveness.VarSet;
+/**
+ * Skeleton class for implementing a faint variable analysis
+ * using the Flow.Analysis interface.
+ */
+public class NullCheckAnalysis implements Flow.Analysis {
+
+    /**
+     * Class for the dataflow objects in the Faintness analysis.
+     * You are free to change this class or move it to another file.
+     */
+    public static class MyDataflowObject implements Flow.DataflowObject {
+        private Set<String> set;
+        public static Set<String> universalSet;
+
+        public boolean contains(String r) {
+        	return set.contains(r);
+        }
+        
+        public MyDataflowObject() {
+            set = new TreeSet<String>();
+        }
+
+        /**
+         * Methods from the Flow.DataflowObject interface. See Flow.java for the
+         * meaning of these methods. These need to be filled in.
+         */
+        public void setToBottom() {
+            set = new TreeSet<String>();
+        }
+
+        
+        public void setToTop() {
+            set = new TreeSet<String>(VarSet.universalSet);
+        }
+
+        public void meetWith(Flow.DataflowObject o) {
+
+            MyDataflowObject a = (MyDataflowObject) o;
+            set.retainAll(a.set);
+        }
+
+        public void copy(Flow.DataflowObject o) {
+            MyDataflowObject a = (MyDataflowObject) o;
+            set = new TreeSet<String>(a.set);
+        }
+
+        @Override
+        public boolean equals(Object o) 
+        {
+            if (o instanceof MyDataflowObject) 
+            {
+                MyDataflowObject a = (MyDataflowObject) o;
+                return set.equals(a.set);
+            }
+            return false;
+        }
+
+        @Override
+        public int hashCode() {
+            return set.hashCode();
+        }
+
+        /**
+         * toString() method for the dataflow objects which is used by
+         * postprocess() below. The format of this method must be of the form
+         * "[ID0, ID1, ID2, ...]", where each ID is the identifier of a quad
+         * defining some register, and the list of IDs must be sorted. See
+         * src/test/test.rd.out for example output of the analysis. The output
+         * format of your reaching definitions analysis must match this exactly.
+         */
+        @Override
+        public String toString() {
+            return set.toString();
+        }
+        
+        public void genVar(String v) {set.add(v);}
+        public void killVar(String v) {set.remove(v);}
+    }
+
+    /**
+     * Dataflow objects for the interior and entry/exit points
+     * of the CFG. in[ID] and out[ID] store the entry and exit
+     * state for the input and output of the quad with identifier ID.
+     *
+     * You are free to modify these fields, just make sure to
+     * preserve the data printed by postprocess(), which relies on these.
+     */
+    private MyDataflowObject[] in, out;
+    private MyDataflowObject entry, exit;
+    
+    /**
+     * This method initializes the datflow framework.
+     *
+     * @param cfg  The control flow graph we are going to process.
+     */
+    public void preprocess(ControlFlowGraph cfg) {
+        // this line must come first.
+        System.out.println("Method: "+cfg.getMethod().getName().toString());
+
+        // get the amount of space we need to allocate for the in/out arrays.
+        QuadIterator qit = new QuadIterator(cfg);
+        int max = 0;
+        while (qit.hasNext()) {
+            int id = qit.next().getID();
+            if (id > max) 
+                max = id;
+        }
+        max += 1;
+
+        // allocate the in and out arrays.
+        in = new MyDataflowObject[max];
+        out = new MyDataflowObject[max];
+        transferfn.val = new MyDataflowObject();
+        // initialize the contents of in and out.
+        qit = new QuadIterator(cfg);
+        while (qit.hasNext()) {
+            int id = qit.next().getID();
+            in[id] = new MyDataflowObject();
+            out[id] = new MyDataflowObject();
+        }
+
+        // initialize the entry and exit points.
+        entry = new MyDataflowObject();
+        exit = new MyDataflowObject();
+
+        /************************************************
+         * Your remaining initialization code goes here *
+         ************************************************/
+        VarSet.universalSet = new TreeSet<String>();
+        int numargs = cfg.getMethod().getParamTypes().length;
+        for (int i = 0; i < numargs; i++) {
+        	VarSet.universalSet.add("R"+i);
+        }
+        
+        qit = new QuadIterator(cfg);
+        while (qit.hasNext()) {
+          Quad q = qit.next();
+          for (RegisterOperand def : q.getDefinedRegisters()) {
+        	  VarSet.universalSet.add(def.getRegister().toString());
+          }
+          for (RegisterOperand use : q.getUsedRegisters()) {
+        	  VarSet.universalSet.add(use.getRegister().toString());
+          }
+          
+        }
+
+    }
+
+    /**
+     * This method is called after the fixpoint is reached.
+     * It must print out the dataflow objects associated with
+     * the entry, exit, and all interior points of the CFG.
+     * Unless you modify in, out, entry, or exit you shouldn't
+     * need to change this method.
+     *
+     * @param cfg  Unused.
+     */
+    public void postprocess (ControlFlowGraph cfg) {
+    	System.out.print(cfg.getMethod().getName() + ":");
+        QuadIterator qit = new QuadIterator(cfg);
+        while (qit.hasNext()) {
+            Quad q = qit.next();
+            if (q.getOperator() instanceof Operator.NullCheck){
+        		for (RegisterOperand use : q.getUsedRegisters()) {
+        			if (((MyDataflowObject) this.getIn(q)).contains(use.getRegister().toString()))
+        			{
+        				System.out.print(q.getID() + " ");
+        				
+        			}
+        				
+        		}
+        	} 
+        }
+    }
+
+    /**
+     * Other methods from the Flow.Analysis interface.
+     * See Flow.java for the meaning of these methods.
+     * These need to be filled in.
+     */
+    /**
+     * Other methods from the Flow.Analysis interface. See Flow.java for the
+     * meaning of these methods. These need to be filled in.
+     */
+    public boolean isForward() {
+        return true;
+    }
+
+    public Flow.DataflowObject getEntry() {
+        Flow.DataflowObject result = newTempVar();
+        result.copy(entry);
+        return result;
+    }
+
+    public Flow.DataflowObject getExit() {
+        Flow.DataflowObject result = newTempVar();
+        result.copy(exit);
+        return result;
+    }
+
+    public void setEntry(Flow.DataflowObject value) {
+        entry.copy(value);
+    }
+
+    public void setExit(Flow.DataflowObject value) {
+        exit.copy(value);
+    }
+
+    public Flow.DataflowObject getIn(Quad q) {
+        Flow.DataflowObject result = newTempVar();
+        result.copy(in[q.getID()]);
+        return result;
+    }
+
+    public Flow.DataflowObject getOut(Quad q) {
+        Flow.DataflowObject result = newTempVar();
+        result.copy(out[q.getID()]);
+        return result;
+    }
+
+    public void setIn(Quad q, Flow.DataflowObject value) {
+        in[q.getID()].copy(value);
+    }
+
+    public void setOut(Quad q, Flow.DataflowObject value) {
+        out[q.getID()].copy(value);
+    }
+
+    public Flow.DataflowObject newTempVar() {
+        return new MyDataflowObject();
+    }
+
+
+    public void processQuad(Quad q) {
+    	transferfn.val.copy(in[q.getID()]);
+        transferfn.visitQuad(q);
+        out[q.getID()].copy(transferfn.val);
+        //System.out.println(val);
+    }
+    private TransferFunction transferfn = new TransferFunction ();
+    public static class TransferFunction extends QuadVisitor.EmptyVisitor {
+    	MyDataflowObject val;
+
+        @Override
+        public void visitQuad(Quad q) {
+        	if (q.getOperator() instanceof Operator.NullCheck){
+        		for (RegisterOperand def : q.getUsedRegisters()) {
+        			val.genVar(def.getRegister().toString());
+        		}
+        	} else {
+        		for (RegisterOperand def : q.getDefinedRegisters()) {
+        			val.killVar(def.getRegister().toString());
+        		}
+        	}
+            
+        }
+    }
+}
